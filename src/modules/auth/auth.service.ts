424a6031d9bae5a2dcffa223d5d3af68cb6ee302
nguyentrinh2611 <nguyentrinha3qt@gmail.com>
2026-03-16 15:09:05 +0700

    refactor: Update authentication module with login functionality, DTOs, and remove unused Prisma files

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async login(data: LoginDto) {
+    return data;
+  }
+}
--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,1 +1,4 @@
-export class LoginDto {}
+export class LoginDto {
+  phone: string;
+  password: string;
+}
