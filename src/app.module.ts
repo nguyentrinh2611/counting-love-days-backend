434511410832cdbfea30c107ae20e73284681244
nguyentrinh2611 <nguyentrinha3qt@gmail.com>
2026-03-16 15:35:28 +0700

    feat: Add @nestjs/config for environment variable management and configure TypeORM with database connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,34 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres' as const,
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT ?? '5432'),
+          username: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          autoLoadEntities: true,
+          synchronize: process.env.TYPEORM_SYNC === 'true',
+        };
+      },
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
