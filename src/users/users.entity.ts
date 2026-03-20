77a4d42b24cb0f57caa38b62f94391b13547b1fc
nguyentrinh2611 <nguyentrinha3qt@gmail.com>
2026-03-20 23:48:17 +0700

    refactor: rename Users entity to User and update its service references.

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,25 +1,31 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
-export class Users {
-    @PrimaryGeneratedColumn()
-    id: number;
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @CreateDateColumn()
-    createdAt: Date;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-    @Column({ default: '' })
-    avatarUrl: string;
+  @Column({ default: '' })
+  avatarUrl: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Users } from './users.entity';
+import { User } from './users.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(Users)
-        private userRepository: Repository<Users>,
-    ) { }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-    async findByEmail(email: string) {
-        return this.userRepository.findOne({ where: { email } });
-    }
+  async findByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
+  }
 
-    async create(userData: Partial<Users>) {
-        const user = this.userRepository.create(userData);
-        return this.userRepository.save(user);
-    }
-
+  async create(userData: Partial<User>) {
+    const user = this.userRepository.create(userData);
+    return this.userRepository.save(user);
+  }
 }
